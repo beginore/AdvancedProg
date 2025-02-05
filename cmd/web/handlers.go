20e2f9ac36357458122f843fb06c465ab7b3880e
BEKSULTAN <230599@astanait.edu.kz>
2025-02-05 16:48:18 +0500

    xgfg

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -311,3 +311,398 @@ func (app *application) postCreateForm(w http.ResponseWriter, r *http.Request) {
 	// Если метод не GET и не POST, возвращаем ошибку
 	app.clientError(w, http.StatusMethodNotAllowed)
 }
+
+func (app *application) userSignup(w http.ResponseWriter, r *http.Request) {
+	var form userSignupForm
+
+	if r.Method == http.MethodGet {
+		data := app.newTemplateData(w, r)
+		data.Form = form
+		app.render(w, http.StatusUnprocessableEntity, "signup.html", data)
+		return
+	}
+
+	if r.Method == http.MethodPost {
+		// Парсим данные из формы
+		err := r.ParseForm()
+		if err != nil {
+			app.clientError(w, http.StatusBadRequest)
+			return
+		}
+		form := userSignupForm{
+			Name:      r.PostForm.Get("name"),
+			Email:     r.PostForm.Get("email"),
+			Password:  r.PostForm.Get("password"),
+			Validator: validator.Validator{},
+		}
+
+		form.CheckField(validator.NotBlank(form.Name), "name", "This field cannot be blank")
+		form.CheckField(validator.NotBlank(form.Email), "email", "This field cannot be blank")
+		form.CheckField(validator.Matches(form.Email, validator.EmailRX), "email", "This field must be a valid email address xxx@xxx.xx")
+		form.CheckField(validator.NotBlank(form.Password), "password", "This field cannot be blank")
+		form.CheckField(validator.MinChars(form.Password, 8), "password", "This field must be at least 8 characters long")
+		form.CheckField(validator.ValidatePassword(form.Password), "password", "Password must contain at least one lowercase letter, one uppercase letter, one digit, and one special character (e.g., @, #, $, %).")
+
+		// Если есть ошибки, возвращаем форму с ошибками
+		if !form.Valid() {
+			data := app.newTemplateData(w, r)
+			data.Form = form
+			app.render(w, http.StatusUnprocessableEntity, "signup.html", data)
+			return
+		}
+
+		// Вставка пользователя в базу данных
+		err = app.users.Insert(form.Name, form.Email, form.Password)
+		if err != nil {
+			// Если ошибка вставки (например, email уже существует)
+			form.AddFieldError("email", "Email address is already in use")
+
+			data := app.newTemplateData(w, r)
+			data.Form = form
+			app.render(w, http.StatusUnprocessableEntity, "signup.html", data)
+			return
+		}
+
+		// Если регистрация прошла успешно, отображаем сообщение и редиректим
+		app.flash(w, r, "Account created successfully!")
+		http.Redirect(w, r, "/user/login", http.StatusSeeOther)
+		return
+	}
+
+	// Для других методов (например, если GET запрос или некорректный метод)
+	app.clientError(w, http.StatusMethodNotAllowed)
+}
+
+func (app *application) userLogin(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodGet {
+		data := app.newTemplateData(w, r)
+		data.Form = userLoginForm{}
+		app.render(w, http.StatusOK, "login.html", data)
+	}
+	if r.Method == http.MethodPost {
+		err := r.ParseForm()
+		if err != nil {
+			app.clientError(w, http.StatusBadRequest)
+			return
+		}
+		form := userSignupForm{
+			Name:      r.PostForm.Get("name"),
+			Email:     r.PostForm.Get("email"),
+			Password:  r.PostForm.Get("password"),
+			Validator: validator.Validator{},
+		}
+
+		form.CheckField(validator.NotBlank(form.Email), "email", "This field cannot be blank")
+		form.CheckField(validator.Matches(form.Email, validator.EmailRX), "email", "This field must be a valid email address")
+		form.CheckField(validator.NotBlank(form.Password), "password", "This field cannot be blank")
+		if !form.Valid() {
+			data := app.newTemplateData(w, r)
+			data.Form = form
+			app.render(w, http.StatusUnprocessableEntity, "login.html", data)
+			return
+		}
+
+		id, err := app.users.Authenticate(form.Email, form.Password)
+		if err != nil {
+			if errors.Is(err, models.ErrInvalidCredentials) {
+				form.AddNonFieldError("Email or password is incorrect")
+				data := app.newTemplateData(w, r)
+				data.Form = form
+				app.render(w, http.StatusUnprocessableEntity, "login.html", data)
+				return
+			} else {
+				app.serverError(w, err)
+				return
+			}
+		}
+
+		app.flash(w, r, "Account logged in successfully!")
+		app.setSession(w, id)
+		http.Redirect(w, r, "/", http.StatusSeeOther)
+	}
+}
+
+func (app *application) userLogout(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	// Renew the session token (invalidate the old session and create a new one).
+	err := app.renewSessionToken(w, r)
+	if err != nil {
+		http.Error(w, "Failed to renew session token", http.StatusUnauthorized)
+		return
+	}
+
+	// Remove the authenticated user ID from the session data.
+	cookie, err := r.Cookie("session_id")
+	if err != nil {
+		http.Error(w, "No session found", http.StatusUnauthorized)
+		return
+	}
+
+	// Remove the session from the session store.
+	delete(app.sessions, cookie.Value)
+
+	// Expire the session cookie on the client side.
+	http.SetCookie(w, &http.Cookie{
+		Name:     "session_id",
+		Value:    "",
+		Path:     "/",
+		MaxAge:   -1,
+		HttpOnly: true,
+		Secure:   true,
+		SameSite: http.SameSiteStrictMode,
+	})
+
+	// Add a flash message for the user.
+	app.flash(w, r, "You've been logged out successfully!")
+
+	// Redirect to the home page.
+	http.Redirect(w, r, "/", http.StatusSeeOther)
+}
+func (app *application) profile(w http.ResponseWriter, r *http.Request) {
+
+	id, err := app.getCurrentUser(r)
+	if err != nil {
+		http.Redirect(w, r, "/", http.StatusFound)
+		return
+	}
+	user, err := app.users.Get(id)
+	if err != nil {
+		app.serverError(w, err)
+		return
+	}
+
+	userPosts, err := app.posts.UserPosts(id)
+	if err != nil {
+		app.serverError(w, err)
+		return
+	}
+
+	userComments, err := app.comments.UserComments(id)
+	if err != nil {
+		app.serverError(w, err)
+		return
+	}
+	data := app.newTemplateData(w, r)
+	data.Posts = userPosts
+	data.Comments = userComments
+	data.User = &models.User{
+		Name:  user.Name,
+		Email: user.Email,
+		Role:  user.Role,
+	}
+
+	app.render(w, http.StatusOK, "profile.html", data)
+}
+
+func (app *application) changePassword(w http.ResponseWriter, r *http.Request) {
+
+	if r.Method != http.MethodPost {
+		app.methodNotAllowed(w)
+		return
+	}
+	id, err := app.getCurrentUser(r)
+	if err != nil {
+		http.Redirect(w, r, "/", http.StatusFound)
+		return
+	}
+	form := passwordForm{
+		CurrentPassword: r.FormValue("currentPassword"),
+		NewPassword:     r.FormValue("newPassword"),
+		ConfirmPassword: r.FormValue("confirmPassword"),
+		Validator:       validator.Validator{},
+	}
+	// fmt.Println(form.CurrentPassword, form.ConfirmPassword, form.NewPassword)
+	form.CheckField(validator.NotBlank(form.CurrentPassword), "currentPassword", "This field cannot be blank")
+	form.CheckField(validator.MinChars(form.CurrentPassword, 8), "currentPassword", "This field must be at least 8 characters long")
+	form.CheckField(validator.NotBlank(form.NewPassword), "newPassword", "This field cannot be blank")
+	form.CheckField(validator.MinChars(form.NewPassword, 8), "newPassword", "This field must be at least 8 characters long")
+	form.CheckField(validator.NotBlank(form.ConfirmPassword), "confirmPassword", "This field cannot be blank")
+	form.CheckField(validator.MinChars(form.ConfirmPassword, 8), "confirmPassword", "This field must be at least 8 characters long")
+	form.CheckField(validator.ComparePassword(form.NewPassword, form.ConfirmPassword), "confirmPassword", "This field must be the same as newPassword")
+
+	if !form.Validator.Valid() {
+		data := app.newTemplateData(w, r)
+		data.Form = form
+		app.render(w, http.StatusUnprocessableEntity, "profile.html", data)
+		return
+	}
+	user, err := app.users.Get(id)
+	if err != nil {
+		app.serverError(w, err)
+		return
+	}
+
+	err = bcrypt.CompareHashAndPassword([]byte(user.HashedPassword), []byte(form.CurrentPassword))
+	if err != nil {
+		app.clientError(w, http.StatusUnauthorized)
+		return
+	}
+
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(form.NewPassword), bcrypt.DefaultCost)
+	if err != nil {
+		app.serverError(w, err)
+		return
+	}
+	err = app.users.UpdatePassword(string(hashedPassword), id)
+	if err != nil {
+		app.serverError(w, err)
+		return
+	}
+	app.flash(w, r, "Password changed successfully!")
+	// Перенаправление на страницу профиля
+	http.Redirect(w, r, "/user/profile/", http.StatusSeeOther)
+}
+
+func (app *application) EditPost(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet && r.Method != http.MethodPost {
+		app.methodNotAllowed(w)
+		return
+	}
+	app.infoLog.Printf("Запрос: Метод: %s, Путь: %s", r.Method, r.URL.Path)
+	if r.Method == http.MethodGet {
+		idParam := r.URL.Query().Get("id")
+		var id int
+		var err error
+
+		if idParam != "" {
+			// Если параметр есть, преобразуем его в число
+			id, err = strconv.Atoi(idParam)
+			if err != nil {
+				app.serverError(w, err)
+			}
+		} else {
+			// Иначе пытаемся извлечь ID из пути
+			path := strings.TrimPrefix(r.URL.Path, "/post/edit/")
+			id, err = strconv.Atoi(path)
+		}
+
+		post, err := app.posts.Get(id)
+		if err != nil {
+			if errors.Is(err, models.ErrNoRecord) {
+				app.notFound(w)
+			} else {
+				app.serverError(w, err)
+			}
+			return
+		}
+
+		data := app.newTemplateData(w, r)
+		data.Form = editPost{
+			ID:        post.ID,
+			Title:     post.Title,
+			Content:   post.Content,
+			ImagePath: post.ImagePath,
+			Category:  post.Category,
+			Author:    post.Author,
+			AuthorID:  post.AuthorID,
+		}
+
+		app.render(w, http.StatusOK, "edit_post.html", data)
+	}
+
+	// Если метод POST, обрабатываем данные формы
+	if r.Method == http.MethodPost {
+		err := r.ParseMultipartForm(20 << 20)
+		if err != nil {
+			app.clientError(w, http.StatusBadRequest)
+			return
+		}
+
+		var filePath string
+		var fileName string
+		file, handler, err := r.FormFile("image")
+
+		if err != nil {
+			if err != http.ErrMissingFile { // Если ошибка не связана с отсутствием файла
+				app.clientError(w, http.StatusBadRequest)
+				return
+			}
+		} else {
+			// Если файл загружен
+			defer file.Close()
+			app.infoLog.Printf("Uploaded File: %+v\n", handler.Filename)
+			app.infoLog.Printf("File Size: %+v\n", handler.Size)
+			app.infoLog.Printf("MIME Header: %+v\n", handler.Header)
+			fileName = fmt.Sprintf("%d-%s", time.Now().UnixNano(), handler.Filename)
+			filePath = fmt.Sprintf("ui/static/upload/%s", fileName)
+			if err := os.MkdirAll("ui/static/upload", os.ModePerm); err != nil {
+				app.serverError(w, err)
+				return
+			}
+			dst, err := os.Create(filePath)
+			if err != nil {
+				app.serverError(w, err)
+				return
+			}
+			defer dst.Close()
+
+			if _, err := io.Copy(dst, file); err != nil {
+				app.serverError(w, err)
+				return
+			}
+		}
+
+		id, err := app.getCurrentUser(r)
+		if err != nil {
+			http.Redirect(w, r, "/", http.StatusFound)
+			return
+		}
+		author, err := app.users.Get(id)
+		if err != nil {
+			app.serverError(w, err)
+			return
+		}
+		strID := r.PostForm.Get("id")
+		intID, err := strconv.Atoi(strID)
+		if err != nil {
+			app.serverError(w, err)
+			return
+		}
+		form := editPost{
+			ID:        intID,
+			Title:     r.PostForm.Get("title"),
+			Content:   r.PostForm.Get("content"),
+			ImagePath: filePath, // Путь к изображению только если файл был загружен
+			Category:  r.PostForm.Get("category"),
+			Author:    author.Name,
+			AuthorID:  author.ID,
+		}
+
+		form.ImagePath = strings.TrimPrefix(form.ImagePath, "ui/static/upload/")
+		// Валидация полей
+		form.CheckField(validator.NotBlank(form.Title), "title", "This field cannot be blank")
+		form.CheckField(validator.MaxChars(form.Title, 100), "title", "This field cannot be longer than 100 characters")
+		form.CheckField(validator.NotBlank(form.Content), "content", "This field cannot be blank")
+		if !form.Valid() {
+			data := app.newTemplateData(w, r)
+			data.Form = form
+			app.render(w, http.StatusUnprocessableEntity, "edit_post.html", data)
+			return
+		}
+
+		// Получаем текущий пост
+
+		post, err := app.posts.Get(form.ID)
+		if err != nil {
+			app.serverError(w, err)
+			return
+		}
+		if form.ImagePath == "" {
+			form.ImagePath = post.ImagePath
+		}
+		app.infoLog.Printf("Updating post: title=%s, content=%s, imagePath=%s, category=%s, author=%s", form.Title, form.Content, form.ImagePath, form.Category, form.Author)
+		err = app.posts.UpdatePost(form.Title, form.Content, form.ImagePath, form.Category, form.Author, form.AuthorID, form.ID)
+		if err != nil {
+			app.serverError(w, err)
+		}
+		app.flash(w, r, "Post edited successfully!")
+		// Перенаправляем на страницу профиля
+		http.Redirect(w, r, fmt.Sprintf("/post/view/%d", form.ID), http.StatusSeeOther)
+		return
+	}
+
+}
